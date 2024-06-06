4d0d9c592c06845d43f65f03897e6b29eab11d1d
Victor Soares <136899628+victorsoaresho@users.noreply.github.com>
2024-06-06 20:31:46 -0300

    asda

--- app.js
+++ app.js
@@ -2,11 +2,39 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const vagasRouter = require('./routes/vagas');
 const usersRouter = require('./routes/users');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API de Vagas',
+      version: '1.0.0',
+      description: 'API para gerenciar vagas e usuários',
+      contact: {
+        name: 'Seu Nome',
+        url: 'http://seusite.com',
+        email: 'seuemail@dominio.com'
+      },
+      servers: [
+        {
+          url: 'http://localhost:8000',
+          description: 'Servidor Local'
+        }
+      ]
+    }
+  },
+  apis: ['./routes/*.js']
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use('/vagas', vagasRouter);
 app.use('/users', usersRouter);

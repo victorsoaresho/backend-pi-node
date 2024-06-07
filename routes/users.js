3dab19009ebe273f43e84bc66b07fabe50c51de5
Victor Soares <136899628+victorsoaresho@users.noreply.github.com>
2024-06-06 21:00:52 -0300

    Doc

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,38 @@ const express = require('express');
 const router = express.Router();
 const { getUserByLogin } = require('../models/user');
 
+/**
+ * @swagger
+ * tags:
+ *   name: Usuários
+ *   description: API para gerenciar usuários
+ */
+
+/**
+ * @swagger
+ * /users/login/{user}/{password}:
+ *   get:
+ *     summary: Faz login do usuário
+ *     tags: [Usuários]
+ *     parameters:
+ *       - in: path
+ *         name: user
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: O login do usuário
+ *       - in: path
+ *         name: password
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: A senha do usuário
+ *     responses:
+ *       200:
+ *         description: Login bem-sucedido
+ *       401:
+ *         description: Falha no login
+ */
 router.get('/login/:user/:password', async (req, res) => {
   try {
     const user = req.params.user;
